eab0ba634f7f941c26ded929dce1976851f37202
ValtierraXavier <xavier.valtierra@icloud.com>
2023-12-19 21:26:18 -0500

    improve speed and memeory of twoSum

--- twoSum.js
+++ twoSum.js
@@ -2,16 +2,16 @@ const numbers = [1, 0, 5, 2, 9, 3, 4, 3, 4, -2, 15]
 const target = 24
 
 const twoSum = (nums, target) => {
-    const copy = [...nums]
     let solution = []
-    for(let j = 0; j < copy.length; j++){
-        for(let k = j + 1; k < copy.length; k++){
-            if(copy[j] + copy[k] === target){
+    for(let j = 0; j < nums.length; j++){
+        for(let k = j + 1; k < nums.length; k++){
+            if(nums[j] + nums[k] === target){
                 solution = [j,k]
+                break;
             }
         }
     }
-    console.log(solution.length > 0?solution:"No Solution Found.")
+    console.log(solution.length > 0 ? solution : "No Solution Found.")
 }
 const twoSum2 = (nums, target) => {
     let complement
@@ -21,7 +21,7 @@ const twoSum2 = (nums, target) => {
         if(nums.indexOf(complement) >= 0 && nums.indexOf(complement) !== i)
         coordinates = [nums.indexOf(complement), i]
     }
-    console.log(coordinates?coordinates:'No solution found.')
+    console.log(coordinates ? coordinates : 'No solution found.')
 }
 twoSum(numbers, target)
 twoSum2(numbers, target)
