bbba40b5e64223e5d2c0e6d3d7745c86c8154076
ValtierraXavier <xavier.valtierra@icloud.com>
2023-12-20 14:32:21 -0500

    Complete minStack problem

--- minStack.js
+++ minStack.js
@@ -9,3 +9,36 @@
 // int getMin() retrieves the minimum element in the stack.
 // You must implement a solution with O(1) time complexity for each function.
 
+
+
+class MinStack {
+    constructor(){
+        this.stack = new Array(-3,-1,0,-2,0);
+    }
+
+    push(val) {
+        this.stack.push(val)
+        return this.stack
+    }
+
+    pop() {
+        this.stack.pop()
+    }
+
+    top() {
+        return this.stack[(this.stack.length -1)]
+    }
+    getMin() {
+        let min = this.stack[0]
+        for(let i = 0; i < this.stack.length; i++){
+            // console.log(min)
+            if(min >= this.stack[i]){
+                min = this.stack[i]
+            }
+        }
+        return min
+    }
+}
+
+const stack = new MinStack()
+console.log(stack.getMin())
